bb0d10c89553aaa88dfefd54a3934b677b462606
PURSUE PANDA <pursuepanda@gmail.com>
2026-08-11 00:11:55 +0530

    Time: 5 ms (81.19%), Space: 46.1 MB (16.64%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        
+        int ori=x;
+        int rev = 0;
+        while(x>0){
+            int digit= x%10;
+            rev = rev*10 +digit;
+            x=x/10;
+
+        }
+        if (ori==rev){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}

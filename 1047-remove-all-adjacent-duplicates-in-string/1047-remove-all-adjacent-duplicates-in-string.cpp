945d387233f9d0adb1d350486da486b9c1ece6a9
PURSUE PANDA <pursuepanda@gmail.com>
2026-09-23 22:20:23 +0530

    Time: 5 ms (60.64%), Space: 14.5 MB (18.67%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        int n =s.size();
+        stack<char> st;
+        int i;
+        string res;
+        for(i=0;i<n;i++){
+            if(st.empty()){
+                st.push(s[i]);
+                continue;
+            }
+            else if(st.top()==s[i]){
+                st.pop();
+                continue;
+            }
+            else{
+                st.push(s[i]);
+            }
+
+        }
+        while(!st.empty()){
+            res.push_back(st.top());
+            st.pop();
+        }
+        reverse(res.begin(),res.end());
+        return res;
+        
+    }
+};  ;
